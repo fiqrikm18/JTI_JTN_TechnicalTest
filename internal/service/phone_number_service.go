7c5c143263b881cd3e33355b280428c198c17813
fiqri khoirul m <fiqrikm18@gmail.com>
2024-01-17 07:29:46 +0700

    feat: phone number service

--- internal/service/phone_number_service.go
+++ internal/service/phone_number_service.go
@@ -3,23 +3,41 @@ package service
 import (
 	"JTI_JTN_TechnicalTest/internal/config"
 	"JTI_JTN_TechnicalTest/internal/model"
+	"JTI_JTN_TechnicalTest/internal/repository"
 )
 
 type PhoneNumberService struct {
-	DBConn *config.DatabaseConn
+	repo *repository.PhoneNumberRepository
 }
 
 func NewPhoneNumberService(conn *config.DatabaseConn) *PhoneNumberService {
-	return &PhoneNumberService{DBConn: conn}
+	repo := repository.NewPhoneNumberRepository(conn.DB)
+	return &PhoneNumberService{repo}
 }
 
 func (service *PhoneNumberService) Create(request model.PhoneNumberRequest) error {
-	//phoneNumber := model.PhoneNumber{PhoneNumber: request.PhoneNumber, Provider: request.Provider}
-	//err := service.DB(phoneNumber)
-	//if err != nil {
-	//	return err
-	//}
+	phoneNumber := model.PhoneNumber{PhoneNumber: request.PhoneNumber, Provider: request.Provider}
+	err := service.repo.Insert(phoneNumber)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (service *PhoneNumberService) CreateBatch(request []model.PhoneNumberRequest) error {
+	phoneNumbers := []model.PhoneNumber{}
+
+	for idx, _ := range phoneNumbers {
+		request := phoneNumbers[idx]
+		phoneNumber := model.PhoneNumber{PhoneNumber: request.PhoneNumber, Provider: request.Provider}
+		phoneNumbers = append(phoneNumbers, phoneNumber)
+	}
+
+	err := service.repo.Insert(phoneNumbers)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
